824dc4709e6ffbe6a0c7f4231c62b7460a835aeb
mkinstitute70-blip <mkinstitute70@gmail.com>
2026-02-28 21:32:22 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+/* firebase.js  (NORMAL COMPAT VERSION — NO MODULE) */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUbxlce-AurocYx2qQ9DW_wwFI4VTHCl0",
+  authDomain: "mk-group-c27c9.firebaseapp.com",
+  projectId: "mk-group-c27c9",
+  storageBucket: "mk-group-c27c9.appspot.com",
+  messagingSenderId: "789213760789",
+  appId: "1:789213760789:web:e54db27b65b8b488ba2d1c"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
