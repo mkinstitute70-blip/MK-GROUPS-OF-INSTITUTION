460551fc762ea38e2898ce39d0ab853208d0ed6f
mkinstitute70-blip <mkinstitute70@gmail.com>
2026-03-01 23:02:07 +0530

    Create notice.js

---
+++ notice.js
@@ -0,0 +1,5 @@
+const noticeData = [
+    { date: "01 March 2026", msg: "New Batch for Sainik School starts from Monday!" },
+    { date: "25 Feb 2026", msg: "Hostel Admissions are now open for session 2026-27." },
+    { date: "20 Feb 2026", msg: "Sunday Mock Test result has been uploaded." }
+];
