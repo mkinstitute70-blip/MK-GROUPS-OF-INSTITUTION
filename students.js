12982f074934047d0d3bdafe8615301aa22785ae
mkinstitute70-blip <mkinstitute70@gmail.com>
2026-03-01 23:22:19 +0530

    Create students.js

---
+++ students.js
@@ -0,0 +1,23 @@
+const studentData = [
+    { 
+        id: "MK-101", 
+        name: "PRIYANSHI LATHWAL", 
+        class: "9th Standard", 
+        paid: 95000, 
+        total: 140000 
+    },
+    { 
+        id: "MK-102", 
+        name: "SAHIL MALIK", 
+        class: "6th Standard", 
+        paid: 60000, 
+        total: 120000 
+    },
+    { 
+        id: "MK-103", 
+        name: "ANKIT HOODA", 
+        class: "9th Standard", 
+        paid: 175000, 
+        total: 175000 
+    }
+];
