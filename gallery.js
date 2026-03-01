d73d7107b9e47a8f3a2f129b42f43047b7f3ea24
mkinstitute70-blip <mkinstitute70@gmail.com>
2026-03-01 23:12:32 +0530

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,5 @@
+const galleryData = [
+    "https://images.unsplash.com/photo-1546410531-bb4caa6b424d",
+    "https://images.unsplash.com/photo-1577891772227-32e58f8995d4",
+    "https://images.unsplash.com/photo-1509062522246-3755977927d7"
+];
