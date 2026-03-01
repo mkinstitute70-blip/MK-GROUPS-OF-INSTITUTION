9608b6a4a57c80935d577cdc2b1fe2721d692431
mkinstitute70-blip <mkinstitute70@gmail.com>
2026-03-01 23:21:20 +0530

    Delete students.js

--- students.js
+++
@@ -1,5 +0,0 @@
-const studentData = [
-    { id: "MK-101", name: "Rahul Kumar", paid: 80000, total: 140000 },
-    { id: "MK-102", name: "Sumit Jha", paid: 155000, total: 155000 },
-    { id: "MK-103", name: "Ankit Hooda", paid: 50000, total: 175000 }
-];
